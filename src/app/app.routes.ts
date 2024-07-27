d711f19628af49eba04d58d9312dc29bc59bcb5c
suneru <suneru89@gmail.com>
2024-07-27 22:48:37 +0530

    Loging Page Add

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,6 @@ import { UserLOginComponent } from './user-login/user-login.component';
 import { PosDashboardComponent } from './pos-dashboard/pos-dashboard.component';
 
 export const routes: Routes = [
-    { path: 'login', component: UserLOginComponent },
+    { path: 'user-login', component: UserLOginComponent },
     { path: 'pos-dashboard', component: PosDashboardComponent }
 ];
--- src/app/pos-dashboard/pos-dashboard.component.ts
+++ src/app/pos-dashboard/pos-dashboard.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { CategoryAddComponent } from '../category-add/category-add.component';
 import { CartComponent } from '../cart/cart.component';
-
+ import { UserLOginComponent } from '../user-login/user-login.component';
 @Component({
   selector: 'app-pos-dashboard',
   standalone: true,
-  imports: [CategoryAddComponent, CartComponent],
+  imports: [CategoryAddComponent, CartComponent,UserLOginComponent],
   templateUrl: './pos-dashboard.component.html',
   styleUrl: './pos-dashboard.component.css'
 })
